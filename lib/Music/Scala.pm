f484f6d728ad1e10471c50e5b3c45bebf44a865a
Jeremy Mates <jmates@cpan.org>
2013-02-19 22:29:24 +0000

    h2xs-5.16 -ACX -b 5.10.0 -c -n Music::Scala --skip-exporter -skip-autoloader

---
+++ lib/Music/Scala.pm
@@ -0,0 +1,80 @@
+package Music::Scala;
+
+use 5.010000;
+use strict;
+use warnings;
+
+our @ISA = qw();
+
+our $VERSION = '0.01';
+
+
+# Preloaded methods go here.
+
+1;
+__END__
+# Below is stub documentation for your module. You'd better edit it!
+
+=head1 NAME
+
+Music::Scala - Perl extension for blah blah blah
+
+=head1 SYNOPSIS
+
+  use Music::Scala;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+Stub documentation for Music::Scala, created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+Blah blah blah.
+
+=head1 HISTORY
+
+=over 8
+
+=item 0.01
+
+Original version; created by h2xs 1.23 with options
+
+  -ACX
+	-b
+	5.10.0
+	-c
+	-n
+	Music::Scala
+	--skip-exporter
+	-skip-autoloader
+
+=back
+
+
+
+=head1 SEE ALSO
+
+Mention other useful documentation such as the documentation of
+related modules or operating system documentation (such as man pages
+in UNIX), or any relevant external documentation such as RFCs or
+standards.
+
+If you have a mailing list set up for your module, mention it here.
+
+If you have a web site set up for your module, mention it here.
+
+=head1 AUTHOR
+
+Jeremy Mates, E<lt>jmates@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2013 by Jeremy Mates
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.16.1 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
