c225a9b01704cca19cdca9bb105b69bfa5957d30
Jeremy Mates <jmates@cpan.org>
2013-02-20 03:04:09 +0000

    Boilerplate foo.

--- lib/Music/Scala.pm
+++ lib/Music/Scala.pm
@@ -1,68 +1,55 @@
+# -*- Perl -*-
+#
+# Scala scale support for Perl - http://www.huygens-fokker.org/scala/
+
 package Music::Scala;
 
 use 5.010000;
 use strict;
 use warnings;
 
-our @ISA = qw();
+use Carp qw/croak/;
 
 our $VERSION = '0.01';
 
+########################################################################
+#
+# SUBROUTINES
 
-# Preloaded methods go here.
+sub new {
+  my ( $class, %param ) = @_;
+  my $self = {};
+  bless $self, $class;
+  return $self;
+}
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
-Music::Scala - Perl extension for blah blah blah
+Music::Scala - Scala scale support for Perl
 
 =head1 SYNOPSIS
 
   use Music::Scala;
-  blah blah blah
+  TODO
 
 =head1 DESCRIPTION
 
-Stub documentation for Music::Scala, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
+Scala scale support for Perl. The L</"SEE ALSO"> section links to
+the developer pages and tuning archive available in the Scala scale
+file format.
 
-Blah blah blah.
+=head1 METHODS
 
-=head1 HISTORY
-
-=over 8
-
-=item 0.01
-
-Original version; created by h2xs 1.23 with options
-
-  -ACX
-	-b
-	5.10.0
-	-c
-	-n
-	Music::Scala
-	--skip-exporter
-	-skip-autoloader
-
-=back
-
-
+TODO
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
+http://www.huygens-fokker.org/scala/ by Manuel Op de Coul.
 
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+http://www.huygens-fokker.org/docs/scales.zip
 
 =head1 AUTHOR
 
@@ -72,9 +59,8 @@ Jeremy Mates, E<lt>jmates@cpan.orgE<gt>
 
 Copyright (C) 2013 by Jeremy Mates
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.16.1 or,
-at your option, any later version of Perl 5 you may have available.
-
+This library is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself, either Perl version 5.16 or, at
+your option, any later version of Perl 5 you may have available.
 
 =cut
