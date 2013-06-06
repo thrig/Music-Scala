32136f7c04c8c0642cfad17a83d3877ccfa873a6
Jeremy Mates <jmates@cpan.org>
2013-06-06 01:35:15 +0000

    Totally not kwalitee whoring.

--- lib/Music/Scala.pm
+++ lib/Music/Scala.pm
@@ -19,7 +19,7 @@ use Carp qw/croak/;
 use File::Basename qw/basename/;
 use Scalar::Util qw/looks_like_number reftype/;
 
-our $VERSION = '0.82';
+our $VERSION = '0.83';
 
 # To avoid file reader from wasting too much time on bum input (longest
 # scala file 'fortune.scl' in archive as of 2013-02-19 has 617 lines).
