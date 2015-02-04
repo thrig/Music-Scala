25ef6f03c1ac2566db094ede899e14a6766f2bc4
Jeremy Mates <jmates@cpan.org>
2015-02-04 02:49:10 +0000

    Relicense under artistic_2.

--- lib/Music/Scala.pm
+++ lib/Music/Scala.pm
@@ -813,10 +813,9 @@ thrig - Jeremy Mates (cpan:JMATES) C<< <jmates at cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2013,2014 by Jeremy Mates
+Copyright (C) 2013-2015 by Jeremy Mates
 
-This library is free software; you can redistribute it and/or modify it under
-the same terms as Perl itself, either Perl version 5.16 or, at your option, any
-later version of Perl 5 you may have available.
+This module is free software; you can redistribute it and/or modify it
+under the Artistic License (2.0).
 
 =cut
